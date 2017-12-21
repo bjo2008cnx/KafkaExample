1e25f9c385d104baed7c10e301878c419ff24e2b
xiaoming.wang <18600882246@163.com>
2017-12-21 18:01:30 +0800

    update

--- demokafka.0.10.1.0/src/main/java/com/jasongj/kafka/producer/HashPartitioner.java
+++ demokafka.0.10.1.0/src/main/java/com/jasongj/kafka/producer/HashPartitioner.java
@@ -1,38 +1,41 @@
 package com.jasongj.kafka.producer;
 
-import java.util.List;
-import java.util.Map;
-
 import org.apache.kafka.clients.producer.Partitioner;
 import org.apache.kafka.common.Cluster;
 import org.apache.kafka.common.PartitionInfo;
 
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 实现HashPartitioner从而保证key相同的消息被发送到同一个Partition
+ */
 public class HashPartitioner implements Partitioner {
 
-	@Override
-	public void configure(Map<String, ?> configs) {
-	}
+    @Override
+    public void configure(Map<String, ?> configs) {
+    }
 
-	@Override
-	public int partition(String topic, Object key, byte[] keyBytes, Object value, byte[] valueBytes, Cluster cluster) {
-		List<PartitionInfo> partitions = cluster.partitionsForTopic(topic);
-		int numPartitions = partitions.size();
-		if (keyBytes != null) {
-			int hashCode = 0;
-			if (key instanceof Integer || key instanceof Long) {
-				hashCode = (int) key;
-			} else {
-				hashCode = key.hashCode();
-			}
-			hashCode = hashCode & 0x7fffffff;
-			return hashCode % numPartitions;
-		} else {
-			return 0;
-		}
-	}
+    @Override
+    public int partition(String topic, Object key, byte[] keyBytes, Object value, byte[] valueBytes, Cluster cluster) {
+        List<PartitionInfo> partitions = cluster.partitionsForTopic(topic);
+        int numPartitions = partitions.size();
+        if (keyBytes != null) {
+            int hashCode = 0;
+            if (key instanceof Integer || key instanceof Long) {
+                hashCode = (int) key;
+            } else {
+                hashCode = key.hashCode();
+            }
+            hashCode = hashCode & 0x7fffffff;
+            return hashCode % numPartitions;
+        } else {
+            return 0;
+        }
+    }
 
-	@Override
-	public void close() {
-	}
+    @Override
+    public void close() {
+    }
 
 }
--- demokafka.0.10.1.0/src/main/java/com/jasongj/kafka/producer/ProducerDemo.java
+++ demokafka.0.10.1.0/src/main/java/com/jasongj/kafka/producer/ProducerDemo.java
@@ -1,17 +1,17 @@
 package com.jasongj.kafka.producer;
 
-import java.util.Properties;
-
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
+import java.util.Properties;
+
 public class ProducerDemo {
 
 	public static void main(String[] args) throws Exception {
 		Properties props = new Properties();
-		props.put("bootstrap.servers", "kafka0:9092");
+		props.put("bootstrap.servers", "localhost:9092");
 		props.put("acks", "all");
 		props.put("retries", 3);
 		props.put("batch.size", 16384);
